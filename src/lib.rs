647111d3e8206b41487398bc4c1b5eed731dd5e0
Kriptikz <cruzxkriptikz@gmail.com>
2022-01-03 13:46:08 +0100

    Defined modules for program architecture in lib.rs Cleaned up unecessary test code

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub mod entrypoint;
+pub mod error;
+pub mod instruction;
+pub mod processor;
+pub mod state;
+
